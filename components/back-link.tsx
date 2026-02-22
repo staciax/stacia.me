9cf438bf6f30f2798a6b49c547254908f5a4e16c
STACiA <stacia.dev@gmail.com>
2026-02-22 18:26:52 +0700

    feat: add not-prose class to BackLink

--- components/back-link.tsx
+++ components/back-link.tsx
@@ -11,7 +11,7 @@ export default function BackLink({ href, label = 'cd ..' }: BackLinkProps) {
       <span className="font-mono opacity-50">&gt; </span>
       <Link
         href={href}
-        className="opacity-50 transition delay-300 duration-300 hover:underline hover:opacity-75"
+        className="not-prose opacity-50 transition delay-300 duration-300 hover:underline hover:opacity-75"
       >
         {label}
       </Link>
