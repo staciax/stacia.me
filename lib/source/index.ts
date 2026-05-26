4ddb58b9add953c3d47cf334013adc6a52a6cece
STACiA <github@stacia.me>
2026-05-27 01:18:59 +0700

    refactor: remove blog date helper functions

--- lib/source/index.ts
+++ lib/source/index.ts
@@ -1,9 +1,4 @@
-import {
-  type InferPageType,
-  loader,
-  PathUtils,
-  update,
-} from 'fumadocs-core/source';
+import { type InferPageType, loader, update } from 'fumadocs-core/source';
 import { toFumadocsSource } from 'fumadocs-mdx/runtime/server';
 import { blog as blogPosts, content } from 'fumadocs-mdx:collections/server';
 
@@ -30,60 +25,6 @@ export const blog = loader(toFumadocsSource(blogPosts, []), {
 });
 
 export type BlogPost = InferPageType<typeof blog>;
-
-const dateOnlyPattern = /^(\d{4})-(\d{2})-(\d{2})$/;
-const blogDateFormatter = new Intl.DateTimeFormat('en-US', {
-  day: 'numeric',
-  month: 'short',
-  timeZone: 'UTC',
-  year: 'numeric',
-});
-
-function getName(path: string) {
-  return PathUtils.basename(path, PathUtils.extname(path));
-}
-
-function parseBlogDate(value: Date | string) {
-  if (value instanceof Date) {
-    return new Date(
-      Date.UTC(value.getUTCFullYear(), value.getUTCMonth(), value.getUTCDate()),
-    );
-  }
-
-  const dateOnly = dateOnlyPattern.exec(value);
-  if (dateOnly) {
-    return new Date(
-      Date.UTC(
-        Number(dateOnly[1]),
-        Number(dateOnly[2]) - 1,
-        Number(dateOnly[3]),
-      ),
-    );
-  }
-
-  const date = new Date(value);
-  if (Number.isNaN(date.getTime())) {
-    throw new Error(`Invalid blog post date: ${value}`);
-  }
-
-  return date;
-}
-
-export function getBlogPostDate(post: BlogPost) {
-  return parseBlogDate(post.data.date ?? getName(post.path));
-}
-
-export function getBlogPostYear(post: BlogPost) {
-  return getBlogPostDate(post).getUTCFullYear();
-}
-
-export function formatBlogPostDate(post: BlogPost) {
-  return blogDateFormatter.format(getBlogPostDate(post));
-}
-
-export function compareBlogPostsByDateDesc(a: BlogPost, b: BlogPost) {
-  return getBlogPostDate(b).getTime() - getBlogPostDate(a).getTime();
-}
 
 export function getPageImage(page: InferPageType<typeof source>) {
   const segments = [...page.slugs, 'image.png'];
