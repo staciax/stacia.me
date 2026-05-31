8bd8d2013e1af53b72628057bb979a4901ccc6da
STACiA <github@stacia.me>
2026-05-31 21:47:18 +0700

    feat: add sitemap route and robots.txt

---
+++ app/sitemap.ts
@@ -0,0 +1,41 @@
+import { baseUrl } from '@/lib/metadata';
+import { source } from '@/lib/source';
+
+import type { MetadataRoute } from 'next';
+
+export const revalidate = false;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = (path: string): string => new URL(path, baseUrl).toString();
+  const items = await Promise.all(
+    source.getPages().map(async (page) => {
+      const { lastModified } = await page.data.load();
+
+      return {
+        url: url(page.url),
+        lastModified: lastModified ? new Date(lastModified) : undefined,
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      } as MetadataRoute.Sitemap[number];
+    }),
+  );
+
+  return [
+    {
+      url: url('/'),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: url('/projects'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: url('/blog'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    ...items.filter((v) => v !== undefined),
+  ];
+}
